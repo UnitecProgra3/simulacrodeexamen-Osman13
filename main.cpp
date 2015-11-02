49f07735a593b232ec3ffe680809804222bca919
Osman13 <osmanoliva13@unitec.edu>
2015-11-02 12:17:25 -0600

    Simulacro Realizado

--- main.cpp
+++ main.cpp
@@ -12,6 +12,8 @@ using namespace std;
 //http://www.cdrummond.qc.ca/cegep/informat/professeurs/alain/images/ASCII1.GIF
 bool esMayuscula(char letra)
 {
+    if(isupper(letra))
+        return true;
     return false;
 }
 
@@ -19,19 +21,32 @@ bool esMayuscula(char letra)
 //Sugerencia, usar string.length() para saber la longitud de la palabra
 bool sonMayusculas(string palabra)
 {
-    return false;
+    int cont = 0;
+    for(int i=0; i<palabra.length();i++){
+        if(!isupper(palabra[i]))
+           return false;
+    }
+    return true;
 }
 
 //Desreferencia num (dado), eleva al cuadrado el numero resultante y lo asigna a la posicion donde num esta apuntando
 void elevarAlCuadrado(int* num)
 {
+    int a = *num;
+    int cuadrado = a*a;
+    *num = cuadrado;
 
 }
 
 //Desreferencia a (dado), b (dado) y c (dado) y devuelve el numero mayor
 int getMayor(int* a, int* b, int* c)
 {
-    return -1;
+    if(*a>*b && *a>*c)
+        return *a;
+    if(*b>*c)
+        return *b;
+    else
+        return *c;
 }
 
 ///////////////////////////////////////////////////////////////////
@@ -49,6 +64,10 @@ public:
     {
         this->num = num;
     }
+    Padre()
+    {
+
+    }
     int getNum()
     {
         return num;
@@ -56,6 +75,26 @@ public:
     void setNum(int num)
     {
         this->num = num;
+    }
+
+};
+
+class Hijo : public Padre
+{
+public:
+    char letra;
+    Hijo(int num, char letra)
+    {
+        this->num = num;
+        this->letra = letra;
+    }
+    char getLetra()
+    {
+        return letra;
+    }
+    void setLetra(char letra)
+    {
+        this->letra = letra;
     }
 };
 
@@ -89,44 +128,44 @@ int main ()
 void evaluar2()
 {
     double nota = 0;
-//    Hijo a(10,'a');
-//    Hijo b(20,'b');
-//    Hijo c(30,'c');
-//    c.setNum(40);
-//    cout<<"Herencia:\t\t";
-//    if(a.getNum()==10
-//        && b.getNum()==20
-//        && c.getNum()==40
-//        )
-//    {
-//        nota+=2.5;
-//        cout<<"Correcto"<<endl;
-//    }else
-//    {
-//        cout<<"Incorrecto"<<endl;
-//    }
-//
-//    c.setLetra('z');
-//    cout<<"Get y sets:\t\t";
-//    if(a.getLetra()=='a'
-//        && b.getLetra()=='b'
-//        && c.getLetra()=='z'
-//        )
-//    {
-//        nota+=2.5;
-//        cout<<"Correcto"<<endl;
-//    }else
-//    {
-//        cout<<"Incorrecto"<<endl;
-//    }
-//
-//    if(a.getNum()==10 && a.getLetra()=='a'
-//        && b.getNum()==20 && b.getLetra()=='b'
-//        && c.getNum()==30 && c.getLetra()=='c'
-//        )
-//    {
-//        nota+=0.5;
-//    }
+    Hijo a(10,'a');
+    Hijo b(20,'b');
+    Hijo c(30,'c');
+    c.setNum(40);
+    cout<<"Herencia:\t\t";
+    if(a.getNum()==10
+        && b.getNum()==20
+        && c.getNum()==40
+        )
+    {
+        nota+=2.5;
+        cout<<"Correcto"<<endl;
+    }else
+    {
+        cout<<"Incorrecto"<<endl;
+    }
+
+    c.setLetra('z');
+    cout<<"Get y sets:\t\t";
+    if(a.getLetra()=='a'
+        && b.getLetra()=='b'
+        && c.getLetra()=='z'
+        )
+    {
+        nota+=2.5;
+        cout<<"Correcto"<<endl;
+    }else
+    {
+        cout<<"Incorrecto"<<endl;
+    }
+
+    if(a.getNum()==10 && a.getLetra()=='a'
+        && b.getNum()==20 && b.getLetra()=='b'
+        && c.getNum()==30 && c.getLetra()=='c'
+        )
+    {
+        nota+=0.5;
+    }
 
     cout<<endl<<"Nota: "<<nota<<"/5"<<endl;
 }
